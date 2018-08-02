ad736a0c7cab88369ee0ba31dee5d1507e6f9207
dussong <dusson@ljll.math.upmc.fr>
2018-08-02 12:39:10 +0100

    temp

--- magma/src/main_run.sh
+++ magma/src/main_run.sh
@@ -5,7 +5,7 @@ source parameters.conf
 . ./script_gen_inv_ba.sh
 
 cat parameters.conf >> julia_run.jl
-cat generate_text_invariants_ba.jl >> julia_run.jl
+cat julia_gen_text_inv_ba.jl >> julia_run.jl
 
 echo "julia run"
 
